f70c7f2a7010609eaf191ad73f902ce9be327e29
Julián Navas Medina <jnavasmedina@safareyes.es>
2026-05-12 12:52:05 +0200

    proyecto service modificado y cambios en estilo de modal

--- frontend/src/app/services/proyecto-service.ts
+++ frontend/src/app/services/proyecto-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiService } from './api.sevice';
-import {proyecto} from "../modelos/proyecto";
+import { proyecto } from '../modelos/proyecto';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +17,17 @@ export class ProyectoService {
 
   getProyectoById(id: number): Observable<proyecto> {
     return this.http.get<proyecto>(`${this.apiService.apiUrl}/proyecto/${id}`);
+  }
+
+  crearProyecto(p: Partial<proyecto>): Observable<proyecto> {
+    return this.http.post<proyecto>(`${this.apiService.apiUrl}/proyecto`, p);
+  }
+
+  actualizarProyecto(id: number, p: Partial<proyecto>): Observable<proyecto> {
+    return this.http.put<proyecto>(`${this.apiService.apiUrl}/proyecto/${id}`, p);
+  }
+
+  eliminarProyecto(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiService.apiUrl}/proyecto/${id}`);
   }
 }
